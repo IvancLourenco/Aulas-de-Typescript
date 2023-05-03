302d7f361c0cceca4b7ab7284714a4224cb6a2b6
Ivan Lourenco <ivancampos039@gmail.com>
2023-05-03 20:44:48 -0300

    arquivo sobre Type aliases adicionado

---
+++ type-aliases/index.ts
@@ -0,0 +1,12 @@
+//vantagem do type aliases é poder fazer uma alteração apenas em um ponto 
+type id = number | string;
+
+let idCustomer: id;
+
+idCustomer = 101
+idCustomer = "huauhasuahus"
+
+let idProduct: id;
+
+idProduct = 198
+idCustomer = "hashashas"
