f7e47b253f52b7a24dab8beb43f674706129bdb4
Ivan Lourenco <ivancampos039@gmail.com>
2023-05-03 20:12:41 -0300

    arquivo sobre como o enum funciona

---
+++ enums/index.ts
@@ -0,0 +1,12 @@
+ enum UserResponse {
+    No = 0,
+    Yes = 1,
+ }
+
+ function respondedEmail(recipient: string, userResponse: UserResponse) : void {
+
+ }
+
+ respondedEmail("Kamila",UserResponse.Yes);
+
+ 
